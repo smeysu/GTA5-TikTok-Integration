ae8af0c587ee86a44dd2811379a81e3f3cc75af7
smeysu <95110801+smeysu@users.noreply.github.com>
2023-07-21 20:29:05 +0200

    Change color

--- Types/Attacker.cs
+++ Types/Attacker.cs
@@ -1,4 +1,4 @@
-﻿using GTA;
+using GTA;
 using GTA.Math;
 using GTA.UI;
 using System;
@@ -62,7 +62,7 @@ namespace GTAVWebhook.Types
             if (npc != null && name != null && World.GetDistance(npc.Position, Game.Player.Character.Position) <= 30 && npc.IsOnScreen)
             {
                 PointF pointF = Screen.WorldToScreen(npc.Position, false);
-                new TextElement(name, pointF, (float)0.6, Color.Red, GTA.UI.Font.Pricedown, Alignment.Center).Draw();
+                new TextElement(name, pointF, (float)0.6, Color.White, GTA.UI.Font.Pricedown, Alignment.Center).Draw();
             }
         }
     }
